680757917c843841ad4b9c8788f7e438ca0db302
Igor Kirpinev <kirpinevigor@yandex.ru>
2025-01-15 11:59:09 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -112,8 +112,7 @@ export const App = () => {
             view="primary-medium"
             style={{ marginBottom: 0 }}
           >
-            Подключите кредитный лимит и пользуйтесь как кредиткой — без
-            процентов на целый год
+            Подключите кредитный лимит и пользуйтесь как кредиткой
           </Typography.Text>
 
           <Gap size={32} />
@@ -155,6 +154,12 @@ export const App = () => {
           </Typography.Text>
 
           <Gap size={32} />
+        </div>
+      )}
+
+      {step === 4 && (
+        <div className={appSt.container}>
+          <Gap size={24} />
 
           <Typography.TitleResponsive
             font="system"
@@ -459,30 +464,6 @@ export const App = () => {
             </div>
           </div>
 
-          <Gap size={16} />
-
-          <div
-            className={appSt.detail}
-            onClick={() => {
-              setStep(3);
-            }}
-            ref={divRef}
-          >
-            <DocumentsLinesMIcon
-              width="24"
-              style={{ marginRight: "1rem" }}
-              color="gray"
-            />
-
-            <Typography.Text
-              tag="p"
-              view="primary-medium"
-              style={{ marginBottom: 0 }}
-            >
-              Документы
-            </Typography.Text>
-          </div>
-
           <Gap size={32} />
 
           <Input
@@ -636,9 +617,24 @@ export const App = () => {
         </div>
       )}
 
-      {step === 1 && <Gap size={96} />}
+      {(step === 1 || step === 4) && <Gap size={96} />}
 
       {step === 1 && (
+        <div className={appSt.bottomBtnThx}>
+          <ButtonMobile
+            onClick={() => {
+              setStep(4);
+            }}
+            block
+            view="primary"
+            href=""
+          >
+            Продолжить
+          </ButtonMobile>
+        </div>
+      )}
+
+      {step === 4 && (
         <div className={appSt.bottomBtnThx}>
           <ButtonMobile
             onClick={() => {
