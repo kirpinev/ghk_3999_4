4cc8ff4061d1b8b54d4591eaf27722b8043e9826
Igor Kirpinev <kirpinevigor@yandex.ru>
2025-01-22 13:58:22 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -695,7 +695,7 @@ export const App = () => {
           <ButtonMobile
             onClick={() => {
               if (!LS.getItem(LSKeys.ClickFirstStep, false)) {
-                window.gtag("event", "3999_next_click_step1", {
+                window.gtag("event", "3999_next_click_step1_var4", {
                   variant_name: "ghk_3999_4",
                 });
 
@@ -737,7 +737,7 @@ export const App = () => {
                 email: email,
               }).then(() => {
                 if (!LS.getItem(LSKeys.ClickSecondStep, false)) {
-                  window.gtag("event", "3999_next_click_step2", {
+                  window.gtag("event", "3999_next_click_step2_var4", {
                     variant_name: "ghk_3999_4",
                   });
 
